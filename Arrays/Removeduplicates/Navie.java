b03be5b0b31c1d9bd3b6b3a42bffc8bbdcfa6262
Shashwat Pal <shashwatpal1021@gmail.com>
2022-04-13 00:03:26 +0530

    remove duplicates

--- Arrays/Removeduplicates/Navie.java
+++ Arrays/Removeduplicates/Navie.java
@@ -1,9 +1,10 @@
 package Arrays.Removeduplicates;
 
-import Arrays.Arrays;
+import java.util.*;
+
 public class Navie {
    public static void main(String[] args) {
-      int[] arr = { 10, 5, 7, 30 };
+      int[] arr = { 10, 20, 20, 30, 30, 30};
       System.out.println("Before Reverse");
 
       System.out.println(Arrays.toString(arr));
@@ -16,6 +17,18 @@ public class Navie {
    }
 
    static int removeDuplicates(int[] arr) {
-      
+      int temp[] = new int[arr.length];
+      temp[0] = arr[0];
+      int res = 1;
+      for (int i = 1; i < temp.length; i++) {
+         if (temp[res - 1] != arr[i]) {
+            temp[res] = arr[i];
+            res++;
+         }
+      }
+      for (int i = 0; i < res; i++) {
+         arr[i] = temp[i];
+      }
+      return res;
    }
 }
